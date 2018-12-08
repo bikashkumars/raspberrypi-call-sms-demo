b1dacbaa75eff0f22645eb7119cf6e6db3190b92
Bikash <bikashkumars@gmail.com>
2018-12-08 12:48:08 +0530

    Phone call and send SMS

---
+++ call.py
@@ -0,0 +1,22 @@
+import serial
+import RPi.GPIO as GPIO      
+import os, time
+
+GPIO.setmode(GPIO.BOARD)    
+
+# Enable Serial Communication
+port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
+
+
+#Call a number and check for status
+networkData = ""
+port.write('ATD9861582056+'+';\r')      # Disable the Echo
+time.sleep(3)
+networkData = port.read(30)
+print networkData
+status = networkData.find("OK")
+if status >=0:
+	print "Calling"
+else:
+	print "Call might fail"
+
